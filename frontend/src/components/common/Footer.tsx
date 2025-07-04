39fa992a9c9360a26001e017f51710eb96d52384
unknown <amssster@gmail.com>
2025-07-04 18:49:24 +0300

    fix(Footer): добавить default export в Footer.tsx

---
+++ frontend/src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center bg-white shadow p-4">
+      <h2 className="text-lg font-semibold">Footer</h2>
+    </footer>
+  );
+}
